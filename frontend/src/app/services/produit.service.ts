3bf727521becf3fa39f8590b60bfe8047a4a5086
Mouloud Ould Ali <mouloudouldali78@gmail.com>
2022-12-29 21:16:49 -0500

    fix produit.service.ts

--- frontend/src/app/services/produit.service.ts
+++ frontend/src/app/services/produit.service.ts
@@ -19,10 +19,10 @@ export class ProduitService {
 
   produits : Produit[]; //un tableau de produits
   //categories : Categorie[];
- 
 
-  constructor(private http : HttpClient) { 
-    
+
+  constructor(private http : HttpClient) {
+
     /* this.categories = [
       {idCat : 1, nomCat : "PC"},
       {idCat : 2, nomCat : "Imprimante"}
@@ -31,10 +31,10 @@ export class ProduitService {
                       categorie : {idCat : 1, nomCat : "PC"} },
                      {idProduit : 2, nomProduit : "Imprimante Epson", prixProduit : 450, dateCreation : new Date("12/17/2010"),
                     categorie :  {idCat : 2, nomCat : "Imprimante"}},
-                     {idProduit : 3, nomProduit :"Tablette Samsung", prixProduit : 900.123, dateCreation : new Date("02/20/2020"), 
+                     {idProduit : 3, nomProduit :"Tablette Samsung", prixProduit : 900.123, dateCreation : new Date("02/20/2020"),
                      categorie : {idCat : 1, nomCat : "PC"}}
                     ];
-    
+
   }
 
   listeProduit(): Observable<Produit[]>{
@@ -50,7 +50,7 @@ export class ProduitService {
         return this.http.delete(url, httpOptions);
         }
 
-        
+
         consulterProduit(id: number): Observable<Produit> {
           const url = `${this.apiURL}/${id}`;
           return this.http.get<Produit>(url);
@@ -67,23 +67,23 @@ export class ProduitService {
             return 0;
           });
           }
-      
+
 
           updateProduit(prod :Produit) : Observable<Produit>
             {
                 return this.http.put<Produit>(this.apiURL, prod, httpOptions);
             }
 
-         
-         
+
+
        listeCategories():Observable<CategorieWrapper>{
             return this.http.get<CategorieWrapper>(this.apiURLCat);
-            }     
+            }
 
   rechercherParCategorie(idCat: number): Observable<Produit[]> {
     const url = `${this.apiURL}/prodscat/${idCat}`;
     return this.http.get<Produit[]>(url);
-  } 
+  }
 
   rechercherParNom(nom: string):Observable< Produit[]> {
     const url = `${this.apiURL}/prodsByName/${nom}`;
@@ -93,7 +93,7 @@ export class ProduitService {
     ajouterCategorie( cat: Categorie):Observable<Categorie>{
       return this.http.post<Categorie>(this.apiURLCat, cat, httpOptions);
       }
-      
 
- 
+
+
 }
