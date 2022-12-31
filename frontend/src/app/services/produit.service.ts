b934196cacf629797c5e34e0032a389862b73a66
Mouloud Ould Ali <mouloudouldali78@gmail.com>
2022-12-30 20:41:16 -0500

    consume APIs with angular

--- frontend/src/app/services/produit.service.ts
+++ frontend/src/app/services/produit.service.ts
@@ -4,6 +4,8 @@ import { Produit } from '../model/produit.model';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CategorieWrapper } from '../model/catgorieWrapped.model';
+import { AuthService } from './auth.service';
+import { apiURL } from '../config';
 
 const httpOptions = {
 headers: new HttpHeaders( {'Content-Type': 'application/json'} )
@@ -14,86 +16,96 @@ headers: new HttpHeaders( {'Content-Type': 'application/json'} )
   providedIn: 'root'
 })
 export class ProduitService {
-  apiURL: string = 'http://localhost:8080/produits/api';
+  
+ 
   apiURLCat: string = 'http://localhost:8080/produits/cat';
 
-  produits : Produit[]; //un tableau de produits
+  produits! : Produit[]; //un tableau de produits
   //categories : Categorie[];
+ 
 
-
-  constructor(private http : HttpClient) {
-
+  constructor(private http : HttpClient,
+              private authService : AuthService) { 
+    
     /* this.categories = [
       {idCat : 1, nomCat : "PC"},
       {idCat : 2, nomCat : "Imprimante"}
     ]; */
-    this.produits = [{idProduit : 1, nomProduit : "PC Asus", prixProduit : 3000.600, dateCreation : new Date("01/14/2011"),
+  /*  this.produits = [{idProduit : 1, nomProduit : "PC Asus", prixProduit : 3000.600, dateCreation : new Date("01/14/2011"),
                       categorie : {idCat : 1, nomCat : "PC"} },
                      {idProduit : 2, nomProduit : "Imprimante Epson", prixProduit : 450, dateCreation : new Date("12/17/2010"),
                     categorie :  {idCat : 2, nomCat : "Imprimante"}},
-                     {idProduit : 3, nomProduit :"Tablette Samsung", prixProduit : 900.123, dateCreation : new Date("02/20/2020"),
+                     {idProduit : 3, nomProduit :"Tablette Samsung", prixProduit : 900.123, dateCreation : new Date("02/20/2020"), 
                      categorie : {idCat : 1, nomCat : "PC"}}
                     ];
-
+                    */
+    
   }
 
   listeProduit(): Observable<Produit[]>{
-    return this.http.get<Produit[]>(this.apiURL);
+    let jwt = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    let httpHeaders = new HttpHeaders({"Authorization":jwt})
+return this.http.get<Produit[]>(apiURL+"/all",{headers:httpHeaders});
+
     }
 
     ajouterProduit( prod: Produit):Observable<Produit>{
-      return this.http.post<Produit>(this.apiURL, prod, httpOptions);
+      let jwt = this.authService.getToken();
+      jwt = "Bearer "+jwt;
+      let httpHeaders = new HttpHeaders({"Authorization":jwt}) 
+        return this.http.post<Produit>(apiURL, prod, {headers:httpHeaders});
       }
-
-      supprimerProduit(id : number) {
-        const url = `${this.apiURL}/${id}`;
-        return this.http.delete(url, httpOptions);
+     
+      
+   
+  supprimerProduit(id : number) {
+        const url = `${apiURL}/${id}`;
+        let jwt = this.authService.getToken();
+        jwt = "Bearer "+jwt;
+        let httpHeaders = new HttpHeaders({"Authorization":jwt}) 
+          return this.http.delete(url,  {headers:httpHeaders});
         }
-
-
-        consulterProduit(id: number): Observable<Produit> {
-          const url = `${this.apiURL}/${id}`;
-          return this.http.get<Produit>(url);
+      
+       consulterProduit(id: number): Observable<Produit> {
+          const url = `${apiURL}/${id}`;
+          let jwt = this.authService.getToken();
+          jwt = "Bearer "+jwt;
+          let httpHeaders = new HttpHeaders({"Authorization":jwt}) 
+            return this.http.get<Produit>(url,{headers:httpHeaders});
           }
-
-          trierProduits(){
-            this.produits = this.produits.sort((n1,n2) => {
-              if (n1.idProduit > n2.idProduit) {
-                  return 1;
-              }
-             if (n1.idProduit < n2.idProduit) {
-                  return -1;
-              }
-            return 0;
-          });
+  
+      updateProduit(prod :Produit) : Observable<Produit>    {
+          let jwt = this.authService.getToken();
+          jwt = "Bearer "+jwt;
+          let httpHeaders = new HttpHeaders({"Authorization":jwt}) 
+            return this.http.put<Produit>(apiURL, prod, {headers:httpHeaders});
           }
+  
 
-
-          updateProduit(prod :Produit) : Observable<Produit>
-            {
-                return this.http.put<Produit>(this.apiURL, prod, httpOptions);
-            }
-
-
-
+         
        listeCategories():Observable<CategorieWrapper>{
-            return this.http.get<CategorieWrapper>(this.apiURLCat);
-            }
+        let jwt = this.authService.getToken();
+        jwt = "Bearer "+jwt;
+        let httpHeaders = new HttpHeaders({"Authorization":jwt})
+        return  this.http.get<CategorieWrapper>(this.apiURLCat,{headers:httpHeaders});
+        
+            }     
 
-  rechercherParCategorie(idCat: number): Observable<Produit[]> {
-    const url = `${this.apiURL}/prodscat/${idCat}`;
-    return this.http.get<Produit[]>(url);
-  }
+       rechercherParCategorie(idCat: number): Observable<Produit[]> {
+          const url = `${apiURL}/prodscat/${idCat}`;
+          return this.http.get<Produit[]>(url);
+         } 
 
   rechercherParNom(nom: string):Observable< Produit[]> {
-    const url = `${this.apiURL}/prodsByName/${nom}`;
+    const url = `${apiURL}/prodsByName/${nom}`;
     return this.http.get<Produit[]>(url);
     }
 
     ajouterCategorie( cat: Categorie):Observable<Categorie>{
       return this.http.post<Categorie>(this.apiURLCat, cat, httpOptions);
       }
+      
 
-
-
+ 
 }
